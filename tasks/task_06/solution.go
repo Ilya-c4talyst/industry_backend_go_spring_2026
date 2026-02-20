c0bc8d5d28600619df3929c4c54f90fff58bf2ec
Ilya Savchenko <c4talyst12@yandex.ru>
2026-02-20 17:30:37 +0300

    done 67

--- tasks/task_06/solution.go
+++ tasks/task_06/solution.go
@@ -7,11 +7,15 @@ type node[K comparable, V any] struct {
 	next  *node[K, V]
 }
 
+type doubleLinkedList[K comparable, V any] struct {
+	head *node[K, V]
+	tail *node[K, V]
+}
+
 type LRUCache[K comparable, V any] struct {
 	capacity int
 	cache    map[K]*node[K, V]
-	head     *node[K, V]
-	tail     *node[K, V]
+	list     doubleLinkedList[K, V]
 }
 
 func NewLRUCache[K comparable, V any](capacity int) *LRUCache[K, V] {
@@ -22,7 +26,7 @@ func NewLRUCache[K comparable, V any](capacity int) *LRUCache[K, V] {
 }
 
 func (l *LRUCache[K, V]) moveToTail(n *node[K, V]) {
-	if n == l.tail {
+	if n == l.list.tail {
 		return
 	}
 
@@ -32,31 +36,31 @@ func (l *LRUCache[K, V]) moveToTail(n *node[K, V]) {
 	if n.next != nil {
 		n.next.prev = n.prev
 	}
-	if n == l.head {
-		l.head = n.next
+	if n == l.list.head {
+		l.list.head = n.next
 	}
-	n.prev = l.tail
+	n.prev = l.list.tail
 	n.next = nil
-	if l.tail != nil {
-		l.tail.next = n
+	if l.list.tail != nil {
+		l.list.tail.next = n
 	}
-	l.tail = n
+	l.list.tail = n
 
-	if l.head == nil {
-		l.head = n
+	if l.list.head == nil {
+		l.list.head = n
 	}
 }
 
 func (l *LRUCache[K, V]) removeLRU() {
-	if l.head == nil {
+	if l.list.head == nil {
 		return
 	}
-	delete(l.cache, l.head.key)
-	l.head = l.head.next
-	if l.head != nil {
-		l.head.prev = nil
+	delete(l.cache, l.list.head.key)
+	l.list.head = l.list.head.next
+	if l.list.head != nil {
+		l.list.head.prev = nil
 	} else {
-		l.tail = nil
+		l.list.tail = nil
 	}
 }
 
